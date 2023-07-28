a95143f397badf1fac597556abaddb6fec49cb55
amanda <amanda@devpipeline.com>
2023-07-27 20:59:21 -0400

    list products

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import { sampleProducts } from './data';
 
 function App() {
     const [count, setCount] = useState(0);
@@ -9,7 +10,17 @@ function App() {
     return (
         <div>
             <header>Cupcake</header>
-            <main></main>
+            <main>
+                <ul>
+                    {sampleProducts.map((product) => (
+                        <li key={product.slug}>
+                            <img src={product.image} alt={product.name} className="product-image" />
+                            <h2>{product.name}</h2>
+                            <p>${product.price}</p>
+                        </li>
+                    ))}
+                </ul>
+            </main>
             <footer>all rights reserved</footer>
         </div>
     );
