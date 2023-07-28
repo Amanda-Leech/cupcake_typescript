29687db8c60c3a64172191539f9b4639a2a38f56
amanda <amanda@devpipeline.com>
2023-07-27 23:28:27 -0400

    create node server

---
+++ backend/src/data.js
@@ -0,0 +1,41 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sampleProducts = void 0;
+exports.sampleProducts = [
+    {
+        name: 'Chocolate Cake Pop',
+        slug: 'chocolate-cake-pop',
+        image: '../images/p1.jpg',
+        catagory: 'Cakepop',
+        cook: 'Bobby',
+        price: 2,
+        countInStock: 24,
+        description: 'Chocolate cake pop in an ice cream cone filled with chocolate candy.',
+        rating: 4.5,
+        numReviews: 6,
+    },
+    {
+        name: 'Strawberry Cake Pop',
+        slug: 'strawberry-cake-pop',
+        image: '../images/p2.jpg',
+        catagory: 'Cakepop',
+        cook: 'Bobby',
+        price: 2,
+        countInStock: 24,
+        description: 'Strawberry cake pop in an ice cream cone filled with chocolate candy.',
+        rating: 4.5,
+        numReviews: 6,
+    },
+    {
+        name: 'Vanilla Cake Pop',
+        slug: 'vanilla-cake-pop',
+        image: '../images/p3.jpg',
+        catagory: 'Cakepop',
+        cook: 'Bobby',
+        price: 2,
+        countInStock: 24,
+        description: 'Vanilla cake pop in an ice cream cone filled with chocolate candy.',
+        rating: 4.5,
+        numReviews: 6,
+    },
+];
---
+++ backend/src/types/Product.ts
@@ -0,0 +1,12 @@
+export type Product = {
+    name: string;
+    slug: string;
+    image: string;
+    catagory: string;
+    cook: string;
+    price: number;
+    countInStock: number;
+    description: string;
+    rating: number;
+    numReviews: number;
+};
